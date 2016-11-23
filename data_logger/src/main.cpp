97294527fd50b99c0be42172fdf098df295b81c6
Jacob Boline <jacob_boline@baylor.edu>
2016-11-22 23:07:49 -0600

    Add and update the IMU code.

--- data_logger/src/main.cpp
+++ data_logger/src/main.cpp
@@ -7,7 +7,7 @@
 #include <SoftwareSerial.h>
 #include "Adafruit_MAX31855.h"
 #include <dht.h>
-//#include <SparkFunLSM9DS1.h>
+#include <SparkFunLSM9DS1.h>
 #include <NewPing.h>
 
 /**********************************GPS*****************************************/
@@ -40,11 +40,14 @@ Adafruit_MAX31855 thermocouple(MAXCS);
 dht DHT;
 
 /**********************************9DoF****************************************/
-//LSM9DS1 imu;
-//#define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
-//#define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
+LSM9DS1 imu;
+#define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
 
-//#define DECLINATION 3.61965
+#define DECLINATION 3.61965
+
+void serial_printAttitude(float, float, float, float, float, float);
+void data_printAttitude(float, float, float, float, float, float);
 
 /**********************************Sonar***************************************/
 #define TRIGGER_PIN  35
@@ -67,6 +70,79 @@ SdFat sd;
 SdFile data;
 
 /*********************************Setup****************************************/
+void setupGyro()
+{
+    imu.settings.gyro.enabled = true;  // Enable the gyro
+    // [scale] sets the full-scale range of the gyroscope.
+    // scale can be set to either 245, 500, or 2000
+    imu.settings.gyro.scale = 2000; // Set scale to +/-2000dps
+    // [sampleRate] sets the output data rate (ODR) of the gyro
+    // sampleRate can be set between 1-6
+    // 1 = 14.9    4 = 238
+    // 2 = 59.5    5 = 476
+    // 3 = 119     6 = 952
+    imu.settings.gyro.sampleRate = 3; // 59.5Hz ODR
+}
+
+void setupAccel()
+{
+    // [enabled] turns the acclerometer on or off.
+    imu.settings.accel.enabled = true; // Enable accelerometer
+    // [enableX], [enableY], and [enableZ] can turn on or off
+    // select axes of the acclerometer.
+    imu.settings.accel.enableX = true; // Enable X
+    imu.settings.accel.enableY = true; // Enable Y
+    imu.settings.accel.enableZ = true; // Enable Z
+    // [scale] sets the full-scale range of the accelerometer.
+    // accel scale can be 2, 4, 8, or 16
+    imu.settings.accel.scale = 16; // Set accel scale to +/-16g.
+    // [highResEnable] enables or disables high resolution
+    // mode for the acclerometer.
+    imu.settings.accel.highResEnable = true; // Disable HR
+}
+
+void setupMag()
+{
+    // [enabled] turns the magnetometer on or off.
+    imu.settings.mag.enabled = true; // Enable magnetometer
+    // [scale] sets the full-scale range of the magnetometer
+    // mag scale can be 4, 8, 12, or 16
+    imu.settings.mag.scale = 16; // Set mag scale to +/-16 Gs
+    // [sampleRate] sets the output data rate (ODR) of the
+    // magnetometer.
+    // mag data rate can be 0-7:
+    // 0 = 0.625 Hz  4 = 10 Hz
+    // 1 = 1.25 Hz   5 = 20 Hz
+    // 2 = 2.5 Hz    6 = 40 Hz
+    // 3 = 5 Hz      7 = 80 Hz
+    imu.settings.mag.sampleRate = 7; // Set OD rate to 80Hz
+    // [tempCompensationEnable] enables or disables
+    // temperature compensation of the magnetometer.
+    imu.settings.mag.tempCompensationEnable = true;
+    // [XYPerformance] sets the x and y-axis performance of the
+    // magnetometer to either:
+    // 0 = Low power mode      2 = high performance
+    // 1 = medium performance  3 = ultra-high performance
+    imu.settings.mag.XYPerformance = 3; // Ultra-high perform.
+    // [ZPerformance] does the same thing, but only for the z
+    imu.settings.mag.ZPerformance = 3; // Ultra-high perform.
+    // [lowPowerEnable] enables or disables low power mode in
+    // the magnetometer.
+    imu.settings.mag.lowPowerEnable = false;
+    // [operatingMode] sets the operating mode of the
+    // magnetometer. operatingMode can be 0-2:
+    // 0 = continuous conversion
+    // 1 = single-conversion
+    // 2 = power down
+    imu.settings.mag.operatingMode = 0; // Continuous mode
+}
+
+void setupTemperature()
+{
+  // [enabled] turns the temperature sensor on or off.
+  imu.settings.temp.enabled = true;
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -80,8 +156,8 @@ void setup()
     sd.errorHalt("opening test.txt for write failed");
   }
 
-  data.println("TIME,SATS,LATITUDE,LONGITUDE,ALT,COURSE,SPEED,PRESSURE,INTTEMP,EXTTEMP,HUMIDITY,PING");
-  Serial.println("TIME,SATS,LATITUDE,LONGITUDE,ALT,COURSE,SPEED,PRESSURE,INTTEMP,EXTTEMP,HUMIDITY,PING");
+  data.println("TIME,SATS,LATITUDE,LONGITUDE,ALT,COURSE,SPEED,PRESSURE,INTTEMP,EXTTEMP,HUMIDITY,GX,GY,GZ,AX,AY,AZ,MX,MY,MZ,PITCH,ROLL,HEADING,PING");
+  Serial.println("TIME,SATS,LATITUDE,LONGITUDE,ALT,COURSE,SPEED,PRESSURE,INTTEMP,EXTTEMP,HUMIDITY,GX,GY,GZ,AX,AY,AZ,MX,MY,MZ,PITCH,ROLL,HEADING,PING");
 
   /////////////////// GPS ///////////////////
   ss.begin(GPSBaud);
@@ -105,11 +181,16 @@ void setup()
   pinMode(DHTVIN, OUTPUT);
   digitalWrite(DHTVIN, HIGH);
 
-  //imu.settings.device.commInterface = IMU_MODE_I2C;
-  //imu.settings.device.mAddress = LSM9DS1_M;
-  //imu.settings.device.agAddress = LSM9DS1_AG;
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
 
-  //imu.begin();
+  setupGyro(); // Set up gyroscope parameters
+  setupAccel(); // Set up accelerometer parameters
+  setupMag(); // Set up magnetometer parameters
+  setupTemperature(); // Set up temp sensor parameter
+
+  imu.begin();
 }
 
 /*********************************LOOP*****************************************/
@@ -122,9 +203,24 @@ void loop()
   DHT.read22(DHTPIN);
 
   // update IMU data
-  //imu.readGyro();
-  //imu.readAccel();
-  //imu.readMag();
+  if (imu.accelAvailable())
+  {
+    imu.readAccel();
+  }
+
+  // imu.gyroAvailable() returns 1 if new gyroscope
+  // data is ready to be read. 0 otherwise.
+  if (imu.gyroAvailable())
+  {
+    imu.readGyro();
+  }
+
+  // imu.magAvailable() returns 1 if new magnetometer
+  // data is ready to be read. 0 otherwise.
+  if (imu.magAvailable())
+  {
+    imu.readMag();
+  }
 
   Serial.print(gps.time.value()); // Raw time in HHMMSSCC format (u32)
   Serial.print(",");
@@ -148,24 +244,26 @@ void loop()
   Serial.print(",");
   Serial.print(DHT.humidity);
   Serial.print(",");
-  //Serial.print(",");
-  //Serial.print(imu.calcGyro(imu.gx), 2);
-  //Serial.print(",");
-  //Serial.print(imu.calcGyro(imu.gy), 2);
-  //Serial.print(",");
-  //Serial.print(imu.calcGyro(imu.gz), 2);
-  //Serial.print(",");
-  //Serial.print(imu.calcAccel(imu.ax), 2);
-  //Serial.print(",");
-  //Serial.print(imu.calcAccel(imu.ay), 2);
-  //Serial.print(",");
-  //Serial.print(imu.calcAccel(imu.az), 2);
-  //Serial.print(",");
-  //Serial.print(imu.calcMag(imu.mx), 2);
-  //Serial.print(",");
-  //Serial.print(imu.calcMag(imu.my), 2);
-  //Serial.print(",");
-  //Serial.println(imu.calcMag(imu.mz), 2);
+  Serial.print(imu.calcGyro(imu.gx), 2);
+  Serial.print(",");
+  Serial.print(imu.calcGyro(imu.gy), 2);
+  Serial.print(",");
+  Serial.print(imu.calcGyro(imu.gz), 2);
+  Serial.print(",");
+  Serial.print(imu.calcAccel(imu.ax), 2);
+  Serial.print(",");
+  Serial.print(imu.calcAccel(imu.ay), 2);
+  Serial.print(",");
+  Serial.print(imu.calcAccel(imu.az), 2);
+  Serial.print(",");
+  Serial.print(imu.calcMag(imu.mx), 2);
+  Serial.print(",");
+  Serial.print(imu.calcMag(imu.my), 2);
+  Serial.print(",");
+  Serial.print(imu.calcMag(imu.mz), 2);
+  Serial.print(",");
+  serial_printAttitude(imu.ax, imu.ay, imu.az, imu.mx, imu.my, imu.mz);
+  Serial.print(",");
   Serial.print(sonar.ping_cm());
   Serial.println("");
 
@@ -191,24 +289,26 @@ void loop()
   data.print(",");
   data.print(DHT.humidity);
   data.print(",");
-  //data.print(",");
-  //data.print(imu.calcGyro(imu.gx), 2);
-  //data.print(",");
-  //data.print(imu.calcGyro(imu.gy), 2);
-  //data.print(",");
-  //data.print(imu.calcGyro(imu.gz), 2);
-  //data.print(",");
-  //data.print(imu.calcAccel(imu.ax), 2);
-  //data.print(",");
-  //data.print(imu.calcAccel(imu.ay), 2);
-  //data.print(",");
-  //data.print(imu.calcAccel(imu.az), 2);
-  //data.print(",");
-  //data.print(imu.calcMag(imu.mx), 2);
-  //data.print(",");
-  //data.print(imu.calcMag(imu.my), 2);
-  //data.print(",");
-  //data.println(imu.calcMag(imu.mz), 2);
+  data.print(imu.calcGyro(imu.gx), 2);
+  data.print(",");
+  data.print(imu.calcGyro(imu.gy), 2);
+  data.print(",");
+  data.print(imu.calcGyro(imu.gz), 2);
+  data.print(",");
+  data.print(imu.calcAccel(imu.ax), 2);
+  data.print(",");
+  data.print(imu.calcAccel(imu.ay), 2);
+  data.print(",");
+  data.print(imu.calcAccel(imu.az), 2);
+  data.print(",");
+  data.print(imu.calcMag(imu.mx), 2);
+  data.print(",");
+  data.print(imu.calcMag(imu.my), 2);
+  data.print(",");
+  data.print(imu.calcMag(imu.mz), 2);
+  data.print(",");
+  data_printAttitude(imu.ax, imu.ay, imu.az, imu.mx, imu.my, imu.mz);
+  data.print(",");
   data.print(sonar.ping_cm());
   data.println("");
 
@@ -232,31 +332,60 @@ static void smartDelay(unsigned long ms)
   } while (millis() - start < ms);
 }
 
-// void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
-// {
-//   float roll = atan2(ay, az);
-//   float pitch = atan2(-ax, sqrt(ay * ay + az * az));
-//
-//   float heading;
-//   if (my == 0)
-//     heading = (mx < 0) ? 180.0 : 0;
-//   else
-//     heading = atan2(mx, my);
-//
-//   heading -= DECLINATION * PI / 180;
-//
-//   if (heading > PI) heading -= (2 * PI);
-//   else if (heading < -PI) heading += (2 * PI);
-//   else if (heading < 0) heading += 2 * PI;
-//
-//   // Convert everything from radians to degrees:
-//   heading *= 180.0 / PI;
-//   pitch *= 180.0 / PI;
-//   roll  *= 180.0 / PI;
-//
-//   Serial.print("Pitch, Roll: ");
-//   Serial.print(pitch, 2);
-//   Serial.print(", ");
-//   Serial.println(roll, 2);
-//   Serial.print("Heading: "); Serial.println(heading, 2);
-// }
+void serial_printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+{
+  float roll = atan2(ay, az);
+  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+
+  float heading;
+  if (my == 0)
+    heading = (mx < 0) ? 180.0 : 0;
+  else
+    heading = atan2(mx, my);
+
+  heading -= DECLINATION * PI / 180;
+
+  if (heading > PI) heading -= (2 * PI);
+  else if (heading < -PI) heading += (2 * PI);
+  else if (heading < 0) heading += 2 * PI;
+
+  // Convert everything from radians to degrees:
+  heading *= 180.0 / PI;
+  pitch *= 180.0 / PI;
+  roll  *= 180.0 / PI;
+
+  Serial.print(pitch, 2);
+  Serial.print(",");
+  Serial.print(roll, 2);
+  Serial.print(",");
+  Serial.print(heading, 2);
+}
+
+void data_printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+{
+  float roll = atan2(ay, az);
+  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+
+  float heading;
+  if (my == 0)
+    heading = (mx < 0) ? 180.0 : 0;
+  else
+    heading = atan2(mx, my);
+
+  heading -= DECLINATION * PI / 180;
+
+  if (heading > PI) heading -= (2 * PI);
+  else if (heading < -PI) heading += (2 * PI);
+  else if (heading < 0) heading += 2 * PI;
+
+  // Convert everything from radians to degrees:
+  heading *= 180.0 / PI;
+  pitch *= 180.0 / PI;
+  roll  *= 180.0 / PI;
+
+  data.print(pitch, 2);
+  data.print(",");
+  data.print(roll, 2);
+  data.print(",");
+  data.print(heading, 2);
+}
